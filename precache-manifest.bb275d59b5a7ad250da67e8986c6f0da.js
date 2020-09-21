e12084ee2c6fb9106d7c83446538f5d90356af62
mkrishna1991 <imkrishna53@gmail.com>
2020-09-21 15:52:52 +0530

    initialcommit

---
+++ precache-manifest.bb275d59b5a7ad250da67e8986c6f0da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25fdd06236dde2bde22d1ff1ea6641a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64e94d53c4fe47a58bb4",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e94d53c4fe47a58bb4",
+    "url": "/static/js/main.88f86926.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
